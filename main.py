b80ed79849cb9ec689e3e80f408440efb1988a94
Darren Sarra <adriansarra18@gmail.com>
2025-05-21 22:55:18 -0400

    Created a main file and created a menu function that has a welcome greeting and a list of items that can be done with the program.

---
+++ main.py
@@ -0,0 +1,12 @@
+def menu():
+    
+    print("Welcome to the Job Application Tracker!\n" \
+    "1. Add new application\n" \
+    "2. View all applications\n" \
+    "3. Update application stauts\n" \
+    "4. Delete an application\n" \
+    "5. Search applications\n" \
+    "0. Exit")
+
+
+menu()
